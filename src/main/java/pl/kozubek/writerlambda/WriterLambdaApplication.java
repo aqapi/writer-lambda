39e9bd9fa16cb6fcf25cabf0cb26a4402fad8a8a
Jacob Kozubek <kozubekjacob@gmail.com>
2022-12-13 16:05:50 +0100

    13.12 fix closed application

--- src/main/java/pl/kozubek/writerlambda/WriterLambdaApplication.java
+++ src/main/java/pl/kozubek/writerlambda/WriterLambdaApplication.java
@@ -33,8 +33,8 @@ public class WriterLambdaApplication {
     private final MeasuringDataService dataService;
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = SpringApplication.run(WriterLambdaApplication.class, args);
-        SpringApplication.exit(ctx, () -> 0);
+        SpringApplication.run(WriterLambdaApplication.class, args);
+        System.exit(0);
     }
 
     @PostConstruct
